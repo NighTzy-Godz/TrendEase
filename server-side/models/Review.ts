fde342ab1cd2980062062f7ae7dbfb7aa56ce6dd
Aser <ajhubero16@gmail.com>
2023-08-18 19:33:39 +0800

    Added the review Model

---
+++ server-side/models/Review.ts
@@ -0,0 +1,51 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+const DB_URL = "mongodb://localhost:27017/trendease";
+(async () => {
+  try {
+    await mongoose.connect(DB_URL);
+    console.log("Connected to the Datebase - Review");
+  } catch (error) {
+    console.log("Error on Review ", error);
+  }
+})();
+
+interface IReview extends Document {
+  productPost: mongoose.Schema.Types.ObjectId;
+  reviewOwner: mongoose.Schema.Types.ObjectId;
+  rating: number;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const reviewSchema: Schema<IReview> = new mongoose.Schema(
+  {
+    productPost: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+
+    reviewOwner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    rating: {
+      type: Number,
+      required: true,
+    },
+
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Review = mongoose.model<IReview>("Review", reviewSchema);
+
+export default Review;
