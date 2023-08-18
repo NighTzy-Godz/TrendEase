0a89911d367055cfc76461c89c4cac17dea76803
Aser <ajhubero16@gmail.com>
2023-08-18 22:20:43 +0800

    Initial Commit for the front end with its folder structure

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="container">
+        <h1>Im the app</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde
+          corrupti molestiae tempore praesentium, blanditiis, dolorum iusto
+          incidunt consectetur voluptatem vero tempora eveniet ea velit fuga,
+          quod distinctio quisquam. Libero ea distinctio blanditiis nemo
+          cupiditate aliquid, a quam atque inventore dolor quod unde qui modi,
+          beatae sit necessitatibus voluptatibus maiores, mollitia eveniet eius
+          in omnis? Eos velit provident esse at, suscipit atque asperiores quia
+          recusandae eligendi, beatae blanditiis quis rerum cupiditate
+          praesentium. Libero optio assumenda, ipsum obcaecati delectus ipsam
+          voluptas. Hic qui quis autem molestiae, magnam velit architecto.
+          Distinctio nam neque, maxime tempore saepe quibusdam cupiditate earum
+          officia, temporibus, laborum dolores.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
