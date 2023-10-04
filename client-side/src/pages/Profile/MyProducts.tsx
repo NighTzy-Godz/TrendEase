f6e5ea9639afdb912b73beea05d06300db3e2c1f
Aser <ajhubero16@gmail.com>
2023-10-04 19:08:16 +0800

    Client Side - Added the pagination on My Products

--- client-side/src/pages/Profile/MyProducts.tsx
+++ client-side/src/pages/Profile/MyProducts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PaddedPage from "../../components/containers/PaddedPage";
 import "../../assets/css/Profile/my_product.css";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,9 +8,14 @@ import { ProductData } from "../../interfaces/product";
 import { State } from "../../store/store";
 import ButtonLink from "../../components/common/ButtonLink";
 import { ButtonSize } from "../../components/common/Button";
+import paginate from "../../utils/paginate";
+import Paginate from "../../components/common/Paginate";
 
 function MyProducts() {
+  const PAGE_LOAD = 8;
   const dispatch = useDispatch();
+
+  const [currPage, setCurrPage] = useState(1);
 
   const myProducts = useSelector(
     (state: State) => state.entities.product.myProducts
@@ -22,13 +27,19 @@ function MyProducts() {
     }
   }, []);
 
-  const renderMyProducts = myProducts?.map((product: ProductData) => {
+  const paginatedProducts = paginate(myProducts as [], currPage, PAGE_LOAD);
+
+  const renderMyProducts = paginatedProducts?.map((product: ProductData) => {
     return (
       <React.Fragment key={product._id}>
         <ProductCard data={product} />
       </React.Fragment>
     );
   });
+
+  const handlePaginateClick = (page: number) => {
+    setCurrPage(page);
+  };
 
   return (
     <PaddedPage className="my_products">
@@ -45,6 +56,12 @@ function MyProducts() {
             <h1>Products You Created</h1>
 
             <div className="product_list">{renderMyProducts}</div>
+            <Paginate
+              currPage={currPage}
+              itemCount={myProducts?.length as number}
+              onPaginateClick={handlePaginateClick}
+              pageLoad={PAGE_LOAD}
+            />
           </>
         )}
       </div>
