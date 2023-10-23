a1e2e52efdf19d1225c5d023352719cbf1057ee9
Aser <ajhubero16@gmail.com>
2023-10-23 23:13:38 +0800

    Client Side - Added the inital design for the home page

--- client-side/src/pages/Home.tsx
+++ client-side/src/pages/Home.tsx
@@ -1,8 +1,29 @@
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { State } from "../store/store";
-import { useEffect } from "react";
 import { getLatestProducts, getTopProducts } from "../store/slices/product";
+import { ProductData } from "../interfaces/product";
+import ProductCard from "../components/product/ProductCard";
+
+import "../assets/css/pages/home.css";
+import LANDING_PNG from "../assets/img/trendease_landing_bg.png";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
+import { State } from "../store/store";
+import ButtonLink from "../components/common/ButtonLink";
+import { ButtonSize } from "../components/common/Button";
+
+const SETTINGS = {
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  autoplay: true,
+};
+
 function Home() {
   const dispatch = useDispatch();
 
@@ -14,8 +35,15 @@ function Home() {
     (state: State) => state.entities.product.topProducts
   );
 
-  console.log(latestProducts);
-  console.log(topProducts);
+  const renderSliderProducts = (products: ProductData[]) => {
+    return products.map((product) => {
+      return (
+        <React.Fragment key={product._id}>
+          <ProductCard data={product} />
+        </React.Fragment>
+      );
+    });
+  };
 
   useEffect(() => {
     dispatch(getLatestProducts());
@@ -23,8 +51,42 @@ function Home() {
   }, []);
 
   return (
-    <div>
-      <h1>Im the Home</h1>
+    <div className="home">
+      <div className="landing_page">
+        <div className="container">
+          <div className="landing_page_left">
+            <div className="text">
+              <h1>Trendease - Where Trends and Ease Unite!</h1>
+              <p>
+                Discover the Latest Trends with TrendEase - Your One-Stop Shop
+                for Fashion, Home, and More. Elevate your style effortlessly.
+              </p>
+            </div>
+            <div className="btn">
+              <ButtonLink size={ButtonSize.LARGE} path="/register">
+                Register
+              </ButtonLink>
+            </div>
+          </div>
+          <div className="landing_page_right">
+            <img src={LANDING_PNG} alt="" loading="lazy" />
+          </div>
+        </div>
+      </div>
+
+      <div className="home_categorized_products">
+        <div className="container">
+          <h1>Latest Products</h1>
+          <Slider {...SETTINGS}>{renderSliderProducts(latestProducts)}</Slider>
+        </div>
+      </div>
+
+      <div className="home_categorized_products">
+        <div className="container">
+          <h1>Top Rated Products</h1>
+          <Slider {...SETTINGS}>{renderSliderProducts(topProducts)}</Slider>
+        </div>
+      </div>
     </div>
   );
 }
